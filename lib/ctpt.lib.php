d33aa7720185b92f05b675f1652686dca3bcaae0
sonikf <93765174+sonikf@users.noreply.github.com>
2022-12-26 03:55:01 +0200

    Update ctpt.lib.php

--- lib/ctpt.lib.php
+++ lib/ctpt.lib.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2022 SuperAdmin
+/* Copyright (C) 2022      Nick Fragoulis
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
