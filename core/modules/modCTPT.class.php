1a4ca0a9ec0562e2a3f68d4471cf6e4086115174
Drosis Nikos <drosis.nikos@gmail.com>
2022-12-27 20:35:03 +0200

    Update modCTPT.class.php

--- core/modules/modCTPT.class.php
+++ core/modules/modCTPT.class.php
@@ -70,10 +70,10 @@ class modCTPT extends DolibarrModules
 
 		// Author
 		$this->editor_name = 'Sonikf';
-		$this->editor_url = '';
+		$this->editor_url = 'https://github.com/sonikf/ctpt';
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.0';
+		$this->version = '1.0.1';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
