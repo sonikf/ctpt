7850f413143174de4ef426f11e6bedc21368b219
Drosis Nikos <drosis.nikos@gmail.com>
2022-12-30 11:11:13 +0200

    Add module id

--- core/modules/modCTPT.class.php
+++ core/modules/modCTPT.class.php
@@ -46,7 +46,7 @@ class modCTPT extends DolibarrModules
 
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-		$this->numero = 500000; // TODO Go on page https://wiki.dolibarr.org/index.php/List_of_modules_id to reserve an id number for your module
+		$this->numero = 428400; // TODO Go on page https://wiki.dolibarr.org/index.php/List_of_modules_id to reserve an id number for your module
 
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'ctpt';
