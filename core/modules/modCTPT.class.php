ee99bbb3a48763e376ee621d815a4b6cf9ba5a76
sonikf <93765174+sonikf@users.noreply.github.com>
2023-10-30 23:15:50 +0200

    1.0.4

--- core/modules/modCTPT.class.php
+++ core/modules/modCTPT.class.php
@@ -73,7 +73,7 @@ class modCTPT extends DolibarrModules
 		$this->editor_url = 'https://github.com/sonikf/ctpt';
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.0.3';
+		$this->version = '1.0.4';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
